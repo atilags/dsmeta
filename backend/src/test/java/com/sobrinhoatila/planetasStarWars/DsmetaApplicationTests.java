b6e0224a552ea8133ff7e2ae9ef67fb536ed9d07
Atila Sobrinho <atilasobrinho@gmail.com>
2022-11-15 18:25:55 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/sobrinhoatila/planetasStarWars/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobrinhoatila.planetasStarWars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
